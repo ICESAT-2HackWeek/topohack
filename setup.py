5e9c11280ea07aa1c694cf62763c817a7e8975dc
Joachim Meyer <jomey@uw.edu>
2019-06-19 13:06:09 +0000

    Add framework for topolib
    Add basic structure to create topolib as a Python package.
    This package will hold any common functions between the notebook.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='topolib',
+      version='0.1',
+      description='Common functions for the TopoHack',
+      author='The team',
+      author_email='email@address.com',
+      url='https://github.com/ICESAT-2HackWeek/topohack',
+      packages=['topolib'],
+     )
